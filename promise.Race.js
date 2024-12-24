b87cd0cfaf6ad6434f227a2b40a323032a64825e
Shadan9113 <mdshadanzaheer@gmail.com>
2024-12-24 14:18:36 +0530

    promise race

---
+++ promise.Race.js
@@ -0,0 +1,12 @@
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, "one");
+  });
+  
+  let promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, "two");
+  });
+  
+  Promise.race([promise1, promise2]).then((value) => {
+    console.log(value); 
+  });
+  
